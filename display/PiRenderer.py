78c004981a431d0e3f5a52d7ceedb50ba5def485
Tim Schonberger <timothy.schonberger@gmail.com>
2020-04-06 16:43:16 -0400

    Implement PiRenderer

---
+++ display/PiRenderer.py
@@ -0,0 +1,69 @@
+import pygame
+
+import os
+from board import SCL, SDA
+import busio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+class PiRenderer:
+
+    def __init__(self):
+        i2c = busio.I2C(SCL, SDA)
+
+        # Create the SSD1306 OLED class.
+        # The first two parameters are the pixel width and pixel height.  Change these
+        # to the right size for your display!
+        self.disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+
+        # Clear display.
+        self.disp.fill(0)
+        self.disp.show()
+
+        # Create blank image for drawing.
+        # Make sure to create image with mode '1' for 1-bit color.
+        self.width = self.disp.width
+        self.height = self.disp.height
+        self.image = Image.new("1", (self.width, self.height))
+
+        # Get drawing object to draw on image.
+        self.draw = ImageDraw.Draw(self.image)
+
+        # Draw a black filled box to clear the image.
+        self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
+
+        # Draw some shapes.
+        # First define some constants to allow easy resizing of shapes.
+        self.padding = -2
+        self.top = self.padding
+        self.bottom = self.height - self.padding
+        # Move left to right keeping track of the current x position for drawing shapes.
+        self.x = 0
+
+        # Load default font.
+        self.font = ImageFont.load_default()
+
+        # Alternatively load a TTF font.  Make sure the .ttf font file is in the
+        # same directory as the python script!
+        # Some other nice fonts to try: http://www.dafont.com/bitmap.php
+        # font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
+
+    def init_renderer(self):
+        os.putenv('DISPLAY', ':0.0')
+        pygame.init()
+
+    def render(self, text_list):
+        self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
+
+        text_y_position = 0
+        if text_list is not None:
+            for tuple in text_list:
+                for text in tuple:
+                    if text is not None:
+                        print(text)
+                        self.draw.text((self.x, self.top + text_y_position * 8), text, font=self.font, fill=255)
+
+                text_y_position = text_y_position + 1
+
+        self.disp.image(self.image)
+        self.disp.show()
