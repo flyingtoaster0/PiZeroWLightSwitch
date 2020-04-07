389676262d3a9d52b7b18f6c364d6ba802befe03
Tim Schonberger <timothy.schonberger@gmail.com>
2020-04-07 16:41:28 -0400

    Write hardcoded GPIO inputs

--- input/GPIOInput.py
+++ input/GPIOInput.py
@@ -6,10 +6,45 @@ from input.InputButton import InputButton
 class GPIOInput:
 
     def __init__(self):
+        self.button_state_map = {
+            InputButton.up: False,
+            InputButton.down: False,
+            InputButton.left: False,
+            InputButton.right: False,
+            InputButton.enter: False,
+            InputButton.back: False,
+        }
+
+        self.gpio_button_map = {
+            4: InputButton.up,
+            17: InputButton.down,
+            18: InputButton.left,
+            27: InputButton.right,
+            22: InputButton.enter,
+            23: InputButton.back,
+        }
+
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(6, GPIO.IN)
+        GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def get_button(self):
-        if GPIO.input(6):
-            return InputButton.up
+
+        for input_pin, button in self.gpio_button_map:
+            if self.check_map(input_pin) is True:
+                return button
+
         return None
+
+    def check_map(self, input_pin):
+        button = self.gpio_button_map[input_pin]
+
+        previous_state = self.button_state_map[button]
+
+        current_button_state = GPIO.input(input_pin) == GPIO.HIGH
+
+        self.button_state_map[button] = current_button_state
+
+        state = self.button_state_map[button]
+
+        return previous_state is False and state is True
+
