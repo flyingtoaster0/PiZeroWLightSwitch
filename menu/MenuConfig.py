4e24afdf6950f3b7c3acec02a7a03d6b208ef05d
Tim Schonberger <timothy.schonberger@gmail.com>
2020-04-08 22:19:39 -0400

    Remove test menus

--- menu/MenuConfig.py
+++ menu/MenuConfig.py
@@ -7,15 +7,7 @@ class MenuConfig:
 
     def get_config(self, repository):
         return {
-            'menu1': NestedMenu('Home', ['den_control', 'room_toggle', 'menu3', 'menu4']),
-            'menu2': NestedMenu('Menu 2', ['menu5', 'menu6', 'menu7']),
-            'menu3': NestedMenu('Menu 3', ['menu5', 'menu6', 'menu7']),
-            'menu4': NestedMenu('Menu 4', ['menu5', 'menu6', 'menu7']),
-            'menu5': NestedMenu('Menu 5', ['menu5', 'menu6', 'menu7']),
-            'menu6': NestedMenu('Menu 6', ['menu5', 'menu6', 'menu7']),
-            'menu7': NestedMenu('Menu 7', ['menu5', 'menu6', 'menu7']),
-            'menu8': NestedMenu('Menu 8', ['menu5', 'menu6', 'menu7']),
-            'menu9': NestedMenu('Menu 9', ['menu5', 'menu6', 'menu7']),
+            'menu1': NestedMenu('Home', ['den_control', 'room_toggle']),
             'room_toggle': HueRoomToggle('Room Toggle', [], repository),
             'den_control': DenApp('Den Control', [], repository)
         }
